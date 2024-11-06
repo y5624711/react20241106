d536a46b4225fb8ec67501e020c093d411a07054
Yun <y5624711@gmail.com>
2024-11-06 10:21:44 +0900

    react 맛보기

---
+++ src/practice/App1.jsx
@@ -0,0 +1,26 @@
+function App1() {
+  return (
+    <div>
+      <h1>Lorem.</h1>
+      <ul>
+        <li>Lorem.</li>
+        <li>Eos!</li>
+        <li>Deserunt?</li>
+        <li>Reprehenderit.</li>
+        <li>At!</li>
+      </ul>
+      <input type="text" />
+      <select name="" id="">
+        <option value="">Lorem.</option>
+        <option value="">Incidunt?</option>
+        <option value="">Tempore.</option>
+        <option value="">Consectetur?</option>
+        <option value="">Et!</option>
+      </select>
+      <br />
+      <button>hello</button>
+    </div>
+  );
+}
+
+export default App1;
