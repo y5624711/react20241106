3558166b710c3b027cab4e78161b10a9cc742301
Yun <y5624711@gmail.com>
2024-11-06 16:36:28 +0900

    chakra ui 설치

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App24 from "./practice/App24.jsx";
+import { Provider } from "./components/ui/provider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App24 />
+    <Provider>
+      <App24 />
+    </Provider>
   </StrictMode>,
 );
