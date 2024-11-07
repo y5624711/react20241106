6828f2b9c0f9f12378f6f888aa053b6afe74cc56
Yun <y5624711@gmail.com>
2024-11-07 10:24:22 +0900

    react 수업

--- src/apps/MyApp28.jsx
+++ src/apps/MyApp28.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import { Input, Textarea } from "@chakra-ui/react";
 import { Field } from "../components/ui/field.jsx";
+import { Checkbox } from "../components/ui/checkbox.jsx";
+import { HiOutlinePlus } from "react-icons/hi";
+import { Radio, RadioGroup } from "../components/ui/radio.jsx";
 
 function MyApp28(props) {
   return (
     <div style={{ margin: "10px" }}>
+      <h5>radio button</h5>
+      <RadioGroup>
+        <Radio value={1}>option1</Radio>
+        <Radio value={2}>option2</Radio>
+        <Radio value={3}>option3</Radio>
+      </RadioGroup>
+      <hr />
+      <h5>checkbox</h5>
+      <Checkbox icon={<HiOutlinePlus />}>동의</Checkbox>
+      <hr />
+      <Checkbox variant="outline" checked>
+        동의
+      </Checkbox>
+      <hr />
+      <Checkbox variant="solid"> 동의 </Checkbox>
+      <hr />
+      <Checkbox variant="subtle"> 동의 </Checkbox>
+      <hr />
+      <input type="checkbox" id="check1" />
+      <label htmlFor="check1">동의</label>
       <hr />
       <Textarea placeholder="자기소개 1000자 제한" variant="flushed"></Textarea>
       <hr />
