e89f050041fc34829e956fd762b3c035d650de4c
Yun <y5624711@gmail.com>
2024-11-11 14:12:22 +0900

    request보내기

--- src/apps/MyApp57.jsx
+++ src/apps/MyApp57.jsx
@@ -1,12 +1,46 @@
 import React from "react";
 import { Button } from "../components/ui/button.jsx";
+import axios from "axios";
 
 function MyApp57(props) {
   return (
     <div>
+      <Button
+        onClick={() => {
+          axios.get("/hi");
+        }}
+      >
+        get 요청
+      </Button>
+      <Button
+        onClick={() => {
+          axios.post("/hello");
+        }}
+      >
+        post 요청
+      </Button>
+      <hr />
       {/*JAVA Script*/}
-      <Button onClick={() => {}}>get 요청</Button>
-      <Button onClick={() => {}}>post 요청</Button>
+      <Button
+        onClick={() => {
+          axios.request({
+            method: "get",
+            url: "/hi",
+          });
+        }}
+      >
+        get 요청
+      </Button>
+      <Button
+        onClick={() => {
+          axios.request({
+            method: "post",
+            url: "/hello",
+          });
+        }}
+      >
+        post 요청
+      </Button>
 
       <hr />
       {/*HTML (a, form)*/}
