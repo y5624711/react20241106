c1fe64b292d88ccbf738929d20b252de728a90db
Yun <y5624711@gmail.com>
2024-11-11 09:37:49 +0900

    context로 prop drilling 없이 데이터 전달

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import MyApp51 from "./apps/MyApp51.jsx";
+import App51 from "./practice/App51.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <Provider>
-    <MyApp51 />
+    <App51 />
     <Toaster />
   </Provider>,
   // </StrictMode>,
---
+++ src/practice/App51.jsx
@@ -0,0 +1,44 @@
+import React, { createContext, useContext, useState } from "react";
+import { Button } from "../components/ui/button.jsx";
+import { Box } from "@chakra-ui/react";
+
+function MyComp1() {
+  return <MyButton />;
+}
+function MyButton() {
+  const count = useContext(CountContext);
+  const updateCount = useContext(UpdateCountContext);
+  return (
+    <Button onClick={() => updateCount(count + 1)}>count : {count}</Button>
+  );
+}
+function MyBox() {
+  const count = useContext(CountContext);
+  return <Box>in box : {count}</Box>;
+}
+function MyComp2() {
+  return <MyBox />;
+}
+
+// step1 : context 만들기
+const CountContext = createContext(0);
+const UpdateCountContext = createContext(null);
+
+function App51(props) {
+  const [count, setCount] = useState(0);
+  const updateCount = (c) => setCount(c);
+
+  return (
+    <div>
+      {/* step3 : 값 전달하기 */}
+      <CountContext.Provider value={count}>
+        <UpdateCountContext.Provider value={updateCount}>
+          <MyComp1 />
+        </UpdateCountContext.Provider>
+        <MyComp2 />
+      </CountContext.Provider>
+    </div>
+  );
+}
+
+export default App51;
