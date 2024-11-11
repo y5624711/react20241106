b18043e7af3c52007eec4cfc75d4dc9aed8ff196
Yun <y5624711@gmail.com>
2024-11-11 11:39:29 +0900

    Strict mode : 개발 시 컴포넌트 마운트 될 때 고의적으로 언마운트, 마운트를 시도한다.

---
+++ src/apps/MyApp55.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { Button } from "../components/ui/button.jsx";
+import { Switch } from "../components/ui/switch.jsx";
+
+function MyApp55() {
+  const [show, setShow] = useState(true);
+
+  return (
+    <>
+      <Switch checked={show} onCheckedChange={(e) => setShow(e.checked)} />
+      {show && <Comp1 />}
+    </>
+  );
+
+  function Comp1(props) {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+      console.log("effect사용", count);
+
+      return () => {
+        console.log("cleanUp", count);
+      };
+    }, [count]);
+
+    return (
+      <div>
+        <Button onClick={() => setCount(count + 1)}>count : {count}</Button>
+      </div>
+    );
+  }
+}
+
+export default MyApp55;
