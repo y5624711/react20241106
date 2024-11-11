afbe4ce140f20ca235008a792c6fb0b5272863bd
Yun <y5624711@gmail.com>
2024-11-11 11:29:09 +0900

    switch

---
+++ src/apps/MyApp52.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Box } from "@chakra-ui/react";
+import { Button } from "../components/ui/button.jsx";
+import { Switch } from "../components/ui/switch.jsx";
+
+//컴포넌트의 life-cycle
+//initial render(mount)
+//re-render(update)
+//unmount
+
+function MyApp52(props) {
+  const [show, setShow] = useState(true);
+  return (
+    <div>
+      {/*umount 후에 initial render 진행*/}
+      <Switch
+        checked={show}
+        onCheckedChange={(e) => setShow(e.checked)}
+      ></Switch>
+      {/*{show && <MyComp1 />}*/}
+      <hr />
+
+      {/*unmount 가 아님*/}
+      <Box display={show ? "block" : "none"}>
+        <MyComp1 />
+      </Box>
+    </div>
+  );
+}
+
+function MyComp1() {
+  const [count, setCount] = useState(0);
+
+  console.log("그림");
+
+  return (
+    // re render - update
+    <Box>
+      <Button onClick={() => setCount(count + 1)}>count : {count}</Button>
+    </Box>
+  );
+}
+export default MyApp52;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import App51 from "./practice/App51.jsx";
+import MyApp54 from "./apps/MyApp54.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <Provider>
-    <App51 />
+    <MyApp54 />
     <Toaster />
   </Provider>,
   // </StrictMode>,
