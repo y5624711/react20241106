29e907f4facb78115fc672ea621ee4a82d82e22f
Yun <y5624711@gmail.com>
2024-11-12 09:56:22 +0900

    Json post 보내기

--- src/apps/MyApp62.jsx
+++ src/apps/MyApp62.jsx
@@ -7,6 +7,48 @@ function MyApp62(props) {
     <div>
       <Button
         onClick={() => {
+          axios.post("/api/main2/sub4", {
+            id: 1,
+            name: "iphone",
+            available: true,
+            shops: [],
+            attribute: {},
+          });
+        }}
+      >
+        btn4
+      </Button>
+
+      <Button
+        onClick={() => {
+          axios.post("/api/main2/sub3", {
+            name: "son",
+            age: 99,
+            married: true,
+            items: ["coffee", "milk", "cola"],
+            team: { name: "토트넘", location: "런던" },
+          });
+        }}
+      >
+        btn3
+      </Button>
+
+      <Button
+        onClick={() => {
+          axios.post("/api/main2/sub2", {
+            title: "소년이온다",
+            author: "한강",
+            content: "내용",
+            price: 10000,
+            quantity: 10,
+          });
+        }}
+      >
+        btn2
+      </Button>
+
+      <Button
+        onClick={() => {
           axios.post("/api/main2/sub1", { name: "son", age: 99 });
         }}
       >
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import App62 from "./practice/App62.jsx";
+import MyApp62 from "./apps/MyApp62.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <App62 />
+      <MyApp62 />
       <Toaster />
     </Provider>
   </StrictMode>,
