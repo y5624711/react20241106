cb081c79c9003e19fee952fc3bfd6d5c8ef8f7a2
Yun <y5624711@gmail.com>
2024-11-12 15:37:17 +0900

    Json 간단한 app

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import MyApp65 from "./apps/MyApp65.jsx";
+import MyApp66 from "./apps/MyApp66.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp65 />
+      <MyApp66 />
       <Toaster />
     </Provider>
   </StrictMode>,
